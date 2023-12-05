a84d77221195a85d6c8ff321dc98b645006d2643
CANguru-System <gustavwostrack@web.de>
2023-12-05 14:39:27 +0100

    New version

--- OTA_include.cpp
+++ OTA_include.cpp
@@ -2,10 +2,10 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <WebServer.h>
-#include "EEPROM.h"
 #include <OTA_include.h>
 #include "ticker.h"
 #include "OWN_LED.h"
+#include "Preferences.h"
 
 #include <ElegantOTA.h>
 
@@ -44,7 +44,7 @@ void timerOTA()
     }
     break;
   case blinkNo:
-      secs = 2;
+    secs = 2;
     break;
   }
   if (secs % 2 == 0)
@@ -58,13 +58,41 @@ void timerOTA()
 // Create AsyncWebServer object on port 80
 WebServer server(80);
 
-void Connect2WiFiandOTA()
+unsigned long ota_progress_millis = 0;
+
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\r\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
+
+void Connect2WiFiandOTA(Preferences preferences)
 {
   tckrOTA.attach(tckrOTATime, timerOTA); // each sec
   blink = blinkFast;
 
-  String ssid = EEPROM.readString(adr_ssid);
-  String password = EEPROM.readString(adr_password);
+  String ssid;
+  ssid = preferences.getString("ssid", "No SSID");
+  String password;
+  password = preferences.getString("password", "No password");
   char ssidCh[ssid.length() + 1];
   ssid.toCharArray(ssidCh, ssid.length() + 1);
   char passwordCh[password.length() + 1];
@@ -81,14 +109,19 @@ void Connect2WiFiandOTA()
 
   // Route for root / web page
   server.on("/", []()
-            { server.send(200, "text/plain", "\r\n\rBitte geben Sie ein:\r\n\r'IP-adresse des Decoders'/update"); });
+            { server.send(200, "text/plain", "\r\n\rCANguru update function\r\n"); });
 
   // Start ElegantOTA
   ElegantOTA.begin(&server);
-  // Start server
+  // ElegantOTA callbacks
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+    // Start server
   server.begin();
   while (true)
   {
-   server.handleClient();
-  } 
+    server.handleClient();
+    ElegantOTA.loop();
+  }
 }
